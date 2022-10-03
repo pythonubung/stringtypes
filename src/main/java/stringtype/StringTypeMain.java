bc3128302c12cfb20e678748231c6d6b312e518c
pythonubung <pythonubung@gmail.com>
2022-10-03 23:09:03 +0200

    String commit1

---
+++ src/main/java/stringtype/StringTypeMain.java
@@ -0,0 +1,12 @@
+package stringtype;
+
+public class StringTypeMain {
+    public static void main(String[] args) {
+        String s1="";
+        String s2="Hello World";
+        String s3="Hello " + "World";
+        System.out.println(s3);
+
+        System.out.println(s2.equals(s3));
+    }
+}
